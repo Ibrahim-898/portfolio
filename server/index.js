ecb02b31b917d9ca2959b4d5e08eb23fa5ba1b26
Ibrahim-898 <ibrahimkhalilulla898@gmail.com>
2026-05-09 00:08:44 +0600

    check route added

--- server/index.js
+++ server/index.js
@@ -9,7 +9,10 @@ const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
-
+//for checking
+app.get('/', (req, res) => {
+  res.send('Portfolio backend running 🚀')
+})
 // Contact form endpoint
 app.post('/api/contact', async (req, res) => {
   const { name, email, message } = req.body
