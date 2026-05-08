8d1d844b37b2ec9cbdfa5109813566f845583570
Ibrahim-898 <ibrahimkhalilulla898@gmail.com>
2026-05-09 00:07:13 +0600

    cors added

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import nodemailer from 'nodemailer'
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(cors({ origin: 'http://localhost:3000' }))
+app.use(cors())
 app.use(express.json())
 
 // Contact form endpoint
